81b154857b684b4fb65b2dca8e7e25cbf6c05549
Hari-01 <haribright01@gmail.com>
2019-11-14 13:50:02 +0530

    U

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const path = require('path');
 
 const app = express();
 
+console.log(express.static('./dist/amirtha-frontend'));
+
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/amirtha-frontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/amirtha-frontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/amirtha-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
